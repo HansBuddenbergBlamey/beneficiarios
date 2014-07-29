974f586b72fc54c44a8eb1b9085140338613885d
user <user@AZ-PC.alico.corp>
2014-07-29 18:17:56 -0400

    WS_BENEF_FATCA

---
+++ src/java/com/globalintegrator/dao/StoreProcedureDAO.java
@@ -0,0 +1,211 @@
+package com.globalintegrator.dao;
+
+import com.globalintegrator.data.SPMapper;
+import com.globalintegrator.model.Beneficiario;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+/**
+ * StoreProcedure DAO - xml and annotation config
+ *
+ * @author Loiane Groner
+ */
+public class StoreProcedureDAO {
+
+    /**
+     *
+     * @param NumPoliza
+     * @param RutBeneficiario
+     * @param NumSiniestro
+     * @param SistemaOrigen
+     * @return
+     */
+    public String insertaBeneficiarioAnnotations(int NumPoliza, String RutBeneficiario, int NumSiniestro, int SistemaOrigen) {
+
+        /**
+         * Llama a session de Base de datos obtenida en
+         * MyBatisConnectionFactory.java.
+         */
+        SqlSessionFactory sqlSessionFactory = MyBatisConnectionFactory.getSqlSessionFactory();
+        SqlSession session = sqlSessionFactory.openSession();
+
+        /**
+         * Llama a la clase SPMapper.java
+         */
+        SPMapper spMapper = session.getMapper(SPMapper.class);
+
+        try {
+            Beneficiario inserta = new Beneficiario();
+            inserta.setNumPoliza(NumPoliza);
+            inserta.setRutBeneficiario(RutBeneficiario);
+            inserta.setNumSiniestro(NumSiniestro);
+            inserta.setFkIdSistOrigen(SistemaOrigen);
+            spMapper.callInsertaBeneficiario(inserta);
+            return inserta.getRes();
+        } finally {
+            session.close();
+        }
+    }
+
+    /* ****************************************************************************************************************** */
+    /**
+     *
+     * @param In_Num_Poliza
+     * @param In_Rut_Beneficiario
+     * @param In_Num_Siniestro
+     * @param In_Num_Propuesta
+     * @param In_Nom1
+     * @param In_Nom2
+     * @param In_Apellido1
+     * @param In_Estado_Usa
+     * @param In_Nacionalidad1
+     * @param In_Nacionalidad2
+     * @param In_Apellido2
+     * @param In_Fecha_Nacimiento
+     * @param In_Ciudad_Usa
+     * @param In_Tin
+     * @param In_Dir_Usa
+     * @param In_Nacionalidad3
+     * @param In_Pais_Nacimiento
+     * @param In_Cod_Postal_Usa
+     * @param In_Fono_Usa
+     * @param In_Envio_Order
+     * @param In_Dir_Cp
+     * @param In_Poder_Notarial
+     * @param In_Ciudad_Cp
+     * @param In_Monto_Poliza
+     * @param In_Cod_Postal_Cp
+     * @param In_Fecha_Form
+     * @param In_Estado_Cp
+     * @return
+     */
+    public String actualizaBeneficiarioAnnotations(int In_Num_Poliza, String In_Rut_Beneficiario, int In_Num_Siniestro, int In_Num_Propuesta, String In_Nom1, String In_Nom2, String In_Apellido1, String In_Apellido2, String In_Nacionalidad1, String In_Nacionalidad2, String In_Nacionalidad3, int In_Fecha_Nacimiento, String In_Pais_Nacimiento, String In_Tin, String In_Dir_Usa, String In_Ciudad_Usa, String In_Estado_Usa, String In_Cod_Postal_Usa, String In_Fono_Usa, int In_Envio_Order, int In_Poder_Notarial, String In_Dir_Cp, String In_Ciudad_Cp, String In_Estado_Cp, String In_Cod_Postal_Cp, int In_Fecha_Form, int In_Monto_Poliza) {
+        /**       
+         * Llama a session de Base de datos obtenida en
+         * MyBatisConnectionFactory.java.
+         */
+        SqlSessionFactory sqlSessionFactory = MyBatisConnectionFactory.getSqlSessionFactory();
+        SqlSession session = sqlSessionFactory.openSession();
+
+        /**
+         * Llama a la clase SPMapper.java
+         */
+        SPMapper spMapper = session.getMapper(SPMapper.class);
+
+        try {
+            Beneficiario actualiza = new Beneficiario();
+            actualiza.setIn_Num_Poliza(In_Num_Poliza);
+            actualiza.setIn_Rut_Beneficiario(In_Rut_Beneficiario);
+            actualiza.setIn_Num_Siniestro(In_Num_Siniestro);
+            actualiza.setIn_Num_Propuesta(In_Num_Propuesta);
+            actualiza.setIn_Nom1(In_Nom1);
+            actualiza.setIn_Nom2(In_Nom2);
+            actualiza.setIn_Apellido1(In_Apellido1);
+            actualiza.setIn_Apellido2(In_Apellido2);
+            actualiza.setIn_Nacionalidad1(In_Nacionalidad1);
+            actualiza.setIn_Nacionalidad2(In_Nacionalidad2);
+            actualiza.setIn_Nacionalidad3(In_Nacionalidad3);
+            actualiza.setIn_Fecha_Nacimiento(In_Fecha_Nacimiento);
+            actualiza.setIn_Pais_Nacimiento(In_Pais_Nacimiento);
+            actualiza.setIn_Tin(In_Tin);
+            actualiza.setIn_Dir_Usa(In_Dir_Usa);
+            actualiza.setIn_Ciudad_Usa(In_Ciudad_Usa);
+            actualiza.setIn_Estado_Usa(In_Estado_Usa);
+            actualiza.setIn_Cod_Postal_Usa(In_Cod_Postal_Usa);
+            actualiza.setIn_Fono_Usa(In_Fono_Usa);
+            actualiza.setIn_Envio_Order(In_Envio_Order);
+            actualiza.setIn_Poder_Notarial(In_Poder_Notarial);
+            actualiza.setIn_Dir_Cp(In_Dir_Cp);
+            actualiza.setIn_Ciudad_Cp(In_Ciudad_Cp);
+            actualiza.setIn_Estado_Cp(In_Estado_Cp);
+            actualiza.setIn_Cod_Postal_Cp(In_Cod_Postal_Cp);
+            actualiza.setIn_Fecha_Form(In_Fecha_Form);
+            actualiza.setIn_Monto_Poliza(In_Monto_Poliza);
+            spMapper.callActualizaBeneficiario(actualiza);
+            return actualiza.getRes();
+        } finally {
+            session.close();
+        }
+    }
+
+    /* ****************************************************************************************************************** */
+
+/**
+     *
+     * @param NumPoliza
+     * @param RutBeneficiario
+     * @param NumSiniestro
+     * @param SistemaOrigen
+     * @return
+     */
+    public String consultaEstadoBeneficiarioAnnotations(int NumPoliza, String RutBeneficiario, int NumSiniestro, int SistemaOrigen) {
+
+        /**
+         * Llama a session de Base de datos obtenida en
+         * MyBatisConnectionFactory.java.
+         */
+        SqlSessionFactory sqlSessionFactory = MyBatisConnectionFactory.getSqlSessionFactory();
+        SqlSession session = sqlSessionFactory.openSession();
+
+        /**
+         * Llama a la clase SPMapper.java
+         */
+        SPMapper spMapper = session.getMapper(SPMapper.class);
+
+        try {
+            Beneficiario consultaEstado = new Beneficiario();
+            consultaEstado.setNumPoliza(NumPoliza);
+            consultaEstado.setRutBeneficiario(RutBeneficiario);
+            consultaEstado.setNumSiniestro(NumSiniestro);
+            consultaEstado.setFkIdSistOrigen(SistemaOrigen);
+            spMapper.callConsultaEstadoBeneficiario(consultaEstado);
+            if ("Encontrado".equals(consultaEstado.getRes()))
+                return consultaEstado.getEstado();
+            else
+                return consultaEstado.getRes();
+        } finally {
+            session.close();
+        }
+    }
+    
+     /**
+     *
+     * @param NumPoliza
+     * @param RutBeneficiario
+     * @param NumSiniestro
+     * @param SistemaOrigen
+     * @param FkIdEstadoNew
+     * @return
+     */
+    public String actualizaEstadoBeneficiarioAnnotations(int NumPoliza, String RutBeneficiario, int NumSiniestro, int SistemaOrigen, int FkIdEstadoNew) {
+
+        /**
+         * Llama a session de Base de datos obtenida en
+         * MyBatisConnectionFactory.java.
+         */
+        SqlSessionFactory sqlSessionFactory = MyBatisConnectionFactory.getSqlSessionFactory();
+        SqlSession session = sqlSessionFactory.openSession();
+
+        /**
+         * Llama a la clase SPMapper.java
+         */
+        SPMapper spMapper = session.getMapper(SPMapper.class);
+
+        try {
+            Beneficiario actualizaEstado = new Beneficiario();
+            actualizaEstado.setNumPoliza(NumPoliza);
+            actualizaEstado.setRutBeneficiario(RutBeneficiario);
+            actualizaEstado.setNumSiniestro(NumSiniestro);
+            actualizaEstado.setFkIdSistOrigen(SistemaOrigen);
+            actualizaEstado.setFkIdEstadoNew(FkIdEstadoNew);
+            spMapper.callActualizaEstadoBeneficiario(actualizaEstado);
+            return actualizaEstado.getRes();
+        } finally {
+            session.close();
+        }
+    }
+
+}
---
+++ src/java/com/globalintegrator/data/SPMapper.java
@@ -0,0 +1,98 @@
+package com.globalintegrator.data;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.mapping.StatementType;
+
+import com.globalintegrator.model.Beneficiario;
+
+/**
+ * Stored Procedure Mapper contains all the myBatis/iBatis annotations
+ * 
+ * @author Loiane Groner
+ * http://loianegroner.com (English)
+ * http://loiane.com (Portuguese)
+ */
+public interface SPMapper {
+
+    /**
+     *
+     * @param beneficiario2 Llama a los objetos declarados en el archivo de parametros Beneficiario.java.
+     * @return
+     */
+    Object callInsertaBeneficiario(Beneficiario beneficiario2);
+
+	//--methos bellow are the same as above, but they are using annotations
+
+    /**
+     *
+     * @param beneficiario2
+     * @return
+     */
+    @Select(value= "{ CALL BENEFICIARIO_FATCA.Inserta_Beneficiario( #{NumPoliza, mode=IN, jdbcType=INTEGER}, #{RutBeneficiario, mode=IN, jdbcType=VARCHAR}, #{NumSiniestro, mode=IN, jdbcType=INTEGER}, #{FkIdSistOrigen, mode=IN, jdbcType=INTEGER}, #{res, mode=OUT, jdbcType=VARCHAR} )}")
+	@Options(statementType = StatementType.CALLABLE)
+	Object callGetInsertaBeneficiarioAnnotations(Beneficiario beneficiario2);
+    
+/*******************************************************************************************************************************/
+
+    /**
+     *
+     * @param beneficiario3 Llama a los objetos declarados en el archivo de parametros Beneficiario.java.
+     * @return
+     */
+    Object callActualizaBeneficiario(Beneficiario beneficiario3);
+
+    //--methos bellow are the same as above, but they are using annotations
+
+    /**
+     *
+     * @param beneficiario3
+     * @return
+     */
+    @Select(value= "{ CALL BENEFICIARIO_FATCA.Update_Beneficiario( #{In_Num_Poliza, mode=IN, jdbcType=NUMBER}, #{In_Rut_Beneficiario, mode=IN, jdbcType=VARCHAR2}, #{In_Num_Siniestro, mode=IN, jdbcType=NUMBER}, #{In_Num_Propuesta, mode=IN, jdbcType=VARCHAR2}, #{In_Nom1, mode=IN, jdbcType=VARCHAR2}, #{In_Nom2, mode=IN, jdbcType=VARCHAR2}, #{In_Apellido1, mode=IN, jdbcType=VARCHAR2}, #{In_Apellido2, mode=IN, jdbcType=VARCHAR2}, #{In_Nacionalidad1, mode=IN, jdbcType=VARCHAR2}, #{In_Nacionalidad2, mode=IN, jdbcType=VARCHAR2}, #{In_Nacionalidad3, mode=IN, jdbcType=VARCHAR2}, #{In_Fecha_Nacimiento, mode=IN, jdbcType=NUMBER}, #{In_Pais_Nacimiento, mode=IN, jdbcType=VARCHAR2}, #{In_Tin, mode=IN, jdbcType=VARCHAR2}, #{In_Dir_Usa, mode=IN, jdbcType=VARCHAR2}, #{In_Ciudad_Usa, mode=IN, jdbcType=VARCHAR2}, #{In_Estado_Usa, mode=IN, jdbcType=VARCHAR2}, #{In_Cod_Postal_Usa, mode=IN, jdbcType=VARCHAR2}, #{In_Fono_Usa, mode=IN, jdbcType=VARCHAR2}, #{In_Envio_Order, mode=IN, jdbcType=NUMBER}, #{In_Poder_Notarial, mode=IN, jdbcType=NUMBER}, #{In_Dir_Cp, mode=IN, jdbcType=VARCHAR2}, #{In_Ciudad_Cp, mode=IN, jdbcType=VARCHAR2}, #{In_Estado_Cp, mode=IN, jdbcType=VARCHAR2}, #{In_Cod_Postal_Cp, mode=IN, jdbcType=VARCHAR2}, #{In_Fecha_Form, mode=IN, jdbcType=NUMBER}, #{In_Monto_Poliza, mode=IN, jdbcType=VARCHAR2}, #{Res, mode=OUT, jdbcType=VARCHAR})}#{In_Num_Poliza, mode=IN, jdbcType=INTEGER}, #{In_Rut_Beneficiario, mode=IN, jdbcType=VARCHAR}, #{In_Num_Siniestro, mode=IN, jdbcType=INTEGER}, #{In_Num_Propuesta, mode=IN, jdbcType=VARCHAR}, #{In_Nom1, mode=IN, jdbcType=VARCHAR}, #{In_Nom2, mode=IN, jdbcType=VARCHAR}, #{In_Apellido1, mode=IN, jdbcType=VARCHAR}, #{In_Apellido2, mode=IN, jdbcType=VARCHAR}, #{In_Nacionalidad1, mode=IN, jdbcType=VARCHAR}, #{In_Nacionalidad2, mode=IN, jdbcType=VARCHAR}, #{In_Nacionalidad3, mode=IN, jdbcType=VARCHAR}, #{In_Fecha_Nacimiento, mode=IN, jdbcType=INTEGER}, #{In_Pais_Nacimiento, mode=IN, jdbcType=VARCHAR}, #{In_Tin, mode=IN, jdbcType=VARCHAR}, #{In_Dir_Usa, mode=IN, jdbcType=VARCHAR}, #{In_Ciudad_Usa, mode=IN, jdbcType=VARCHAR}, #{In_Estado_Usa, mode=IN, jdbcType=VARCHAR}, #{In_Cod_Postal_Usa, mode=IN, jdbcType=VARCHAR}, #{In_Fono_Usa, mode=IN, jdbcType=VARCHAR}, #{In_Envio_Order, mode=IN, jdbcType=INTEGER}, #{In_Poder_Notarial, mode=IN, jdbcType=INTEGER}, #{In_Dir_Cp, mode=IN, jdbcType=VARCHAR}, #{In_Ciudad_Cp, mode=IN, jdbcType=VARCHAR}, #{In_Estado_Cp, mode=IN, jdbcType=VARCHAR}, #{In_Cod_Postal_Cp, mode=IN, jdbcType=VARCHAR}, #{In_Fecha_Form, mode=IN, jdbcType=INTEGER}, #{In_Monto_Poliza, mode=IN, jdbcType=VARCHAR}, #{Res, mode=OUT, jdbcType=VARCHAR})}")
+    @Options(statementType = StatementType.CALLABLE)
+    Object callGetActualizaBeneficiarioAnnotations(Beneficiario beneficiario3);
+        
+/*******************************************************************************************************************************/
+
+    /**
+     *
+     * @param beneficiario4 Llama a los objetos declarados en el archivo de parametros Beneficiario.java.
+     * @return
+     */
+    Object callConsultaEstadoBeneficiario(Beneficiario beneficiario4);
+
+    //--methos bellow are the same as above, but they are using annotations
+
+    /**
+     *
+     * @param beneficiario4
+     * @return
+     */
+    @Select(value= "{ CALL BENEFICIARIO_FATCA.Consulta_Estado_Beneficiario( #{NumPoliza, mode=IN, jdbcType=INTEGER}, #{RutBeneficiario, mode=IN, jdbcType=VARCHAR}, #{NumSiniestro, mode=IN, jdbcType=INTEGER}, #{FkIdSistOrigen, mode=IN, jdbcType=INTEGER}, #{Res, mode=OUT, jdbcType=VARCHAR}, #{Estado, mode=OUT, jdbcType=VARCHAR})}")
+    @Options(statementType = StatementType.CALLABLE)
+    Object callGetConsultaEstadoBeneficiarioAnnotations(Beneficiario beneficiario4);
+        
+    /*******************************************************************************************************************************/
+
+    /**
+     *
+     * @param beneficiario5 Llama a los objetos declarados en el archivo de parametros Beneficiario.java.
+     * @return
+     */
+    Object callActualizaEstadoBeneficiario(Beneficiario beneficiario5);
+
+    //--methos bellow are the same as above, but they are using annotations
+
+    /**
+     *
+     * @param beneficiario5
+     * @return
+     */
+    @Select(value= "{ CALL BENEFICIARIO_FATCA.Update_Estado_Beneficiario( #{NumPoliza, mode=IN, jdbcType=INTEGER}, #{RutBeneficiario, mode=IN, jdbcType=VARCHAR}, #{NumSiniestro, mode=IN, jdbcType=INTEGER}, #{FkIdSistOrigen, mode=IN, jdbcType=INTEGER}, #{FkIdEstadoNew, mode=IN, jdbcType=INTEGER}, #{res, mode=OUT, jdbcType=VARCHAR} )}")
+    @Options(statementType = StatementType.CALLABLE)                      
+    Object callGetActualizaEstadoBeneficiarioAnnotations(Beneficiario beneficiario5);
+}
